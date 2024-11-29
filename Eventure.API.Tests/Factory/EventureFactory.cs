3eb131153a78e9c230650854cca4bea52fe18a63
Selen1s <romanskypotter@gmail.com>
2024-11-30 00:08:08 +0300

    Добавлена фабрика

---
+++ Eventure.API.Tests/Factory/EventureFactory.cs
@@ -0,0 +1,16 @@
+﻿using AuthService.Application.Contracts;
+
+namespace Eventure.API.Tests.Factory;
+
+public static class EventureFactory
+{
+    public static LoginUserRequest GetLoginUserRequest()
+    {
+        return new LoginUserRequest("321321@mail.ru", "vasya2004");
+    }
+
+    public static RegisterUserRequest GetRegisterUserRequest()
+    {
+        return new RegisterUserRequest("vasya", "321321@mail.ru", "vasya2004");
+    }
+}
